e0776f284b0b62bab1fff1c5245d925b9eb599b4
yobwee-zy <bkingeno07@gmail.com>
2022-11-17 09:13:49 +0000

    Amenity

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
